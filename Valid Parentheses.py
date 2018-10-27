9ecdaadda2e6fc8e293e5e8a9cc19bae762b8a22
liushengwen <liushengwen@360.cn>
2018-10-27 20:37:41 +0800

    有效括号

---
+++ Valid Parentheses.py
@@ -0,0 +1,12 @@
+def isValid(s):
+	stack = []
+	paren_map = {')':'(',']':'[','}':'{'}
+	for c in s:
+	    if c not in paren_map:
+	         stack.append(c)
+	    elif not stack or paren_map[c] != stack.pop():
+	         return False
+
+	return not stack
+
+print isValid("[]}")
