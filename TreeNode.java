f140ab6df5b3c3d4e364801a3ce38007fe94384c
大道 <lswdonald9@gmail.com>
2018-11-18 10:46:38 +0800

    二叉树

---
+++ TreeNode.java
@@ -0,0 +1,19 @@
+package com.algorithm.treenode;
+
+public class TreeNode extends Object{
+
+	int val;
+	
+	//左孩子
+	TreeNode left;
+	
+	//右孩子
+	TreeNode right;
+	
+	
+	public String toString() {
+		
+		
+        return "( val:" + val + " left:" + left + " right:" + right +")";
+    }
+}
